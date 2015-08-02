b2402ae6a8774d8f1333c5fa66d3c381ceafcf6f
Mykhailo Shulichenko <Mykhailo.Shulichenko@portaone.com>
2015-08-02 18:30:42 +0300

    Rewritten to use with xtrabackup

---
+++ README.md
@@ -0,0 +1,4 @@
+MySQL backup script, based on percona xtrabackup tool
+
+- Create full backup once a month
+- Make incremental backups every day
--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,68 +1,56 @@
-#!/bin/bash
-# MySQL backup script
-# Author: Michael Shulichenko
+#!/bin/bash -x
 
-USER="root"
-PASSWD="rootpassword"
-CHARSET="utf8"
-DATADIR="/backup"
+# Based on percona xtrabackup tool
+# https://www.percona.com/doc/percona-xtrabackup/2.2/howtos/recipes_xbk_inc.html
 
-FTP_ADDR="my.ftpserver.net"
-FTP_USER="anonymous"
-FTP_PASS="mypass"
-FTP_PATH="/Backup" 
+BACKUP_DIR="/backup/mysql";
 
-DATABASES=$1
+#**********************************************************************
 
-PREFIX=`date +%F`
+DAY_OF_MONTH=$(date +%d);
+STORAGE_DIR=$(date +%Y_%B);
 
-function usage {
-    echo Usage:
-    echo `basename $0` dbname1,dbname2,dbname3,...
+TARGET_DIR="$BACKUP_DIR/$STORAGE_DIR/$DAY_OF_MONTH";
+INCREMENTAL_BASEDIR="$BACKUP_DIR/$STORAGE_DIR/incremental-basedir";
+
+function raiseError {
+    case "$1" in
+        "601" ) echo "Can't complete task: Directory creation failed.";;
+        "602" ) echo "Can't complete task: Disk space is low";;
+        "603" ) echo "Can't complete task: Target directory already exists";;
+        *     ) echo "Can't complete task: Unknown error occured";;
+    esac;
+    exit 1;
+}
+
+function createFullBackup {
+    xtrabackup --backup --target-dir=${TARGET_DIR}
+    ln -s ${TARGET_DIR} ${INCREMENTAL_BASEDIR};
+}
+
+function createIncrementBackup {
+    xtrabackup --backup --target-dir=${TARGET_DIR} \
+      --incremental-basedir=${INCREMENTAL_BASEDIR}
+}
+
+function checkCreateDir {
+    if [ ! -d "$1" ]; then
+        mkdir -p $1;
+        if [ "$?" != "0" ]; then
+            raiseError 601;
+        fi;
+    else
+        raiseError 603;
+    fi;
+}
+
+checkCreateDir ${TARGET_DIR};
+
+if [ "$DAY_OF_MONTH" == "01" ] || [ ! -L "${INCREMENTAL_BASEDIR}" ]; then
+    createFullBackup;
     exit 0;
-}
+fi;
 
-function upload {
-    FILE=$1
-    echo "[++++++----][`date +%F--%H-%M`] Uploading backup to FTP."
-    wput --basename=$DATADIR -u -q -n 5 -B ${FILE} ftp://${FTP_USER}:${FTP_PASS}@${FTP_ADDR}${FTP_PATH}/
-    echo "[++++++++--][`date +%F--%H-%M`] Uploading backup to FTP - successfull."
-}
-
-function do_backup {
-        DBNAME=$1
-        mysqldump --user=$USER --host=$HOST --password=$PASSWD --default-character-set=$CHARSET $DBNAME \
-        | bzip2 -9 > $DATADIR/$PREFIX/DB/$DBNAME-`date +%F--%H-%M`.sql.bz2
-}
-
-#-----------------------------------------------------------------------
-
-if [ -z "${DATABASES}" ]
-then
-    usage
-fi
-
-mkdir -p $DATADIR/$PREFIX/DB;
-
-# Split databases by ','
-IFS=',' read -ra DBNAME <<< "$DATABASES"
-
-echo "[--------------------------------[`date +%F--%H-%M`]--------------------------------]" 
-echo "[----------][`date +%F--%H-%M`] Run the backup script..."
-echo "[++--------][`date +%F--%H-%M`] Generate a database backup..."
-
-# Create backup for each database
-for db in "${DBNAME[@]}"; do
-    do_backup $db
-done;
-
-echo "[++++------][`date +%F--%H-%M`] Backup database - successfull."
-
-# Upload all backups to ftp server
-upload $DATADIR/$PREFIX/DB
-
-echo "[+++++++++-][`date +%F--%H-%M`] Free HDD space: `df -h /home|tail -n1|awk '{print $4}'`"
-echo "[++++++++++][`date +%F--%H-%M`] All operations completed successfully!"
+createIncrementBackup;
 
 exit 0;
-
